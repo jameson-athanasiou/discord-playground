99b08a224dc9b36ad4d9d0edfc3b065b1bdc40d3
Jameson Athanasiou <jamesonathanasiou@gmail.com>
2023-03-17 19:54:41 -0400

    call out to reddit

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 mod settings;
 
+use html_parser;
+use reqwest;
 use serenity::async_trait;
 use serenity::framework::standard::macros::{command, group};
 use serenity::framework::standard::{CommandResult, StandardFramework};
 use serenity::model::channel::Message;
 use serenity::prelude::*;
 use settings::Settings;
+use tl;
 
 #[group]
-#[commands(ping)]
+#[commands(ping, deals)]
 struct General;
 
 struct Handler;
@@ -18,6 +21,8 @@ impl EventHandler for Handler {}
 
 #[tokio::main]
 async fn main() {
+    // lookup golf deals on reddit
+
     println!("Hello, world!");
 
     let settings = Settings::new().unwrap();
@@ -25,7 +30,7 @@ async fn main() {
     println!("{}", settings.discord.token);
 
     let framework = StandardFramework::new()
-        .configure(|c| c.prefix("~")) // set the bot's prefix to "~"
+        .configure(|c| c.prefix("!")) // set the bot's prefix to "~"
         .group(&GENERAL_GROUP);
 
     let token = settings.discord.token;
@@ -42,9 +47,41 @@ async fn main() {
     }
 }
 
+async fn get_todays_deals() {
+    let url =
+        "https://www.reddit.com/r/golf/comments/11trhpq/daily_golf_deals_03172023_nurseresidences/";
+
+    let res = reqwest::Client::new()
+        .get(url)
+        .send()
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap();
+
+    let dom = tl::parse(&res, tl::ParserOptions::default()).unwrap();
+    let parser = dom.parser();
+    let element = dom
+        .get_elements_by_class_name("usertext-body")
+        .last()
+        .expect("Failed to find element")
+        .get(parser)
+        .unwrap();
+
+    println!("{:?}", element);
+}
+
 #[command]
 async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
     msg.reply(ctx, "Pong!").await?;
+
+    Ok(())
+}
+
+#[command]
+async fn deals(ctx: &Context, msg: &Message) -> CommandResult {
+    get_todays_deals().await;
 
     Ok(())
 }
